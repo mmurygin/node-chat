2806dad447b4ba3cf83e6841d4e0fda5dcdb2dd8
Murygin Maksim <muriginm@gmail.com>
2017-11-06 12:14:38 +0700

    set version to v1.1.0

--- public/main.js
+++ public/main.js
@@ -225,7 +225,7 @@ $(function() {
   socket.on('login', function () {
     connected = true;
     // Display the welcome message
-    var message = 'Welcome to Socket.IO Chat v1.0.0';
+    var message = 'Welcome to Socket.IO Chat v1.1.0';
     log(message, {
       prepend: true
     });
